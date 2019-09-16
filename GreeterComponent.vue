a06e63fd4475ca99fc69ed5efc0f0ae051dd102e
Albert Brand <abrand@xebia.com>
2019-09-16 20:38:23 +0200

    Add greeter component

---
+++ GreeterComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hello!</h1>
+    <p>See you soon 👋</p>
+  </div>
+</template>
+
+<style scoped>
+h1,
+p {
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+}
+</style>
